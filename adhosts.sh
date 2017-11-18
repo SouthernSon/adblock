01d5c2d87f154b04a8a0d6178c5be4c1fb5a3630
SouthernSon <33071270+SouthernSon@users.noreply.github.com>
2017-11-18 16:27:50 -0600

    Update adhosts.sh

--- adhosts.sh
+++ adhosts.sh
@@ -1,29 +1,35 @@
 #!/bin/sh 
-
 #---Ad block script for DD-WRT using dnsmasq
 
-TMPPATH=/tmp/tmphosts
-HOSTPATH=/tmp/adhosts
-CONFPATH=/tmp/dnsmasq.conf
+hostspath="/tmp"
+tmpfile="$hostspath/tmphosts"
+hostfile="$hostspath/adhosts"
+conffile="$hostspath/dnsmasq.conf"
+logfile="$hostspath/adblock.log"
 
-{
-wget -O - 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext'
-wget -O - 'http://www.mvps.org/winhelp2002/hosts.txt'
-wget -O - http://www.malwaredomainlist.com/hostslist/hosts.txt
-} > $TMPPATH
+>$tmpfile
+>$hostfile
+>$logfile
 
-#Format downloaded lists for hosts file.
-sed -e 's/#.*$//' -e 's/127.0.0.1/0.0.0.0/g' | grep 0.0.0.0 | grep -v localhost | sort | uniq -u > $HOSTPATH
+sleep 20
 
-rm $TMPPATH
+wget -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext" >> $tmpfile 2>>$logfile
+wget -O - "http://www.mvps.org/winhelp2002/hosts.txt" >> $tmpfile 2>>$logfile
+wget -O - "http://www.malwaredomainlist.com/hostslist/hosts.txt" >> $tmpfile 2>>$logfile
 
-#Example below removes sites you may need from the list, add other sites accordingly.
-sed -i /"adf.ly"/d "$HOSTPATH"
 
-#Add entry to dnsmasq conf file.
-echo "addn-hosts=$HOSTPATH" >> $CONFPATH
+sed -e 's/#.*$//' -e 's/127.0.0.1/0.0.0.0/g' $tmpfile | grep 0.0.0.0 | 
+grep -v localhost | sort | uniq -u >> $hostfile
 
-#Kill and reload dnsmasq. These are the options DD-WRT uses at startup.
+#rm $tmpfile
+
+sed -i /"adf.ly"/d "$hostfile"
+
+echo "addn-hosts=$hostfile" >> $conffile
+
+#Kill and reload dnsmasq. For some reason if user not specified dnsmasq will
+#start as nobody (cron?). Must explicitly define user,group, and config file. 
+#These are the options DD-WRT uses at startup
 killall dnsmasq
 sleep 1
-dnsmasq -u root -g root --conf-file=$CONFPATH
+dnsmasq -u root -g root --conf-file=$conffile
