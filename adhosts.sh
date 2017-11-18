d4a1b0934156a066ec740650d916ca3f67428222
SouthernSon <33071270+SouthernSon@users.noreply.github.com>
2017-11-18 16:33:39 -0600

    Update adhosts.sh

--- adhosts.sh
+++ adhosts.sh
@@ -11,24 +11,29 @@ logfile="$hostspath/adblock.log"
 >$hostfile
 >$logfile
 
+#Wget will fail during startup when under load
+#adjust sleep time below for your router
 sleep 20
 
+#Add additional sources for ad providers here
 wget -O - "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext" >> $tmpfile 2>>$logfile
 wget -O - "http://www.mvps.org/winhelp2002/hosts.txt" >> $tmpfile 2>>$logfile
 wget -O - "http://www.malwaredomainlist.com/hostslist/hosts.txt" >> $tmpfile 2>>$logfile
 
-
+#Format lists for hosts file and remove comments, create hosts file
+#and remove temp file
 sed -e 's/#.*$//' -e 's/127.0.0.1/0.0.0.0/g' $tmpfile | grep 0.0.0.0 | 
 grep -v localhost | sort | uniq -u >> $hostfile
 
-#rm $tmpfile
+rm $tmpfile
 
+#Remove hosts from the list you may need
 sed -i /"adf.ly"/d "$hostfile"
 
 echo "addn-hosts=$hostfile" >> $conffile
 
 #Kill and reload dnsmasq. For some reason if user not specified dnsmasq will
-#start as nobody (cron?). Must explicitly define user,group, and config file. 
+#start as nobody. Must explicitly define user,group, and config file. 
 #These are the options DD-WRT uses at startup
 killall dnsmasq
 sleep 1
